ce13e744821f49310503e17faf721f6857398fd8
Sarthak <sarthakkalpande113@gmail.com>
2021-12-04 01:24:47 +0530

    react app created with basic api structure

---
+++ Music_Streaming_Rooms/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Music_Streaming_Rooms project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Music_Streaming_Rooms.settings')
+
+application = get_asgi_application()
