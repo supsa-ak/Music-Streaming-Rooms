0e8c9c8b3517e7e8854d26928e6d99d118119eb1
Sarthak <sarthakkalpande113@gmail.com>
2021-12-11 21:52:38 +0530

    api completed with basic requirements

--- api/views.py
+++ api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+    
+    def post(self, request, format=None):
+        pass
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <h1>Testing React Code</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-
 const appDiv = document.getElementById("app");
-render(<App/>, appDiv);f
+render(<App />, appDiv);
