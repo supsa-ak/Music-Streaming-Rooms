b8fcef4f031922895022a482706be6cecdd974f9
Sarthak <sarthakkalpande113@gmail.com>
2021-12-12 20:22:08 +0530

    Homepage Designed and user will be redirected to  previous room using session when opening homepage

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
@@ -69,3 +70,12 @@ class JoinRoom(APIView):
             return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
 
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
