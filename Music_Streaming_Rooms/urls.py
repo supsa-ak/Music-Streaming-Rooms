15addf5066cb32cec4f92eb1f3b7c2459e6c4037
Sarthak <sarthakkalpande113@gmail.com>
2021-12-15 02:45:17 +0530

    created spotify app and done all stuff related to spotify one issue is after signing in it is not going to accept page and then redirecting to room page as it is http develpment server

--- Music_Streaming_Rooms/urls.py
+++ Music_Streaming_Rooms/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('frontend.urls')),
+    path('spotify/', include('spotify.urls')),
 ]
